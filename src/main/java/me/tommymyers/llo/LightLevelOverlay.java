5d2de155b57310f14a9a2aa85d3291d746c145da
Tommy Myers <tommymyers1101@gmail.com>
2019-05-13 18:47:56 +0100

    Clean up unused code

--- src/main/java/me/tommymyers/llo/LightLevelOverlay.java
+++ src/main/java/me/tommymyers/llo/LightLevelOverlay.java
@@ -1,40 +1,23 @@
 package me.tommymyers.llo;
 
-import java.awt.Color;
-
-import com.mojang.blaze3d.platform.GlStateManager;
+import org.lwjgl.glfw.GLFW;
 
 import me.shedaniel.cloth.hooks.ClothClientHooks;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.keybinding.FabricKeyBinding;
 import net.fabricmc.fabric.impl.client.keybinding.KeyBindingRegistryImpl;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.client.render.BufferBuilder;
 import net.minecraft.client.render.Camera;
-import net.minecraft.client.render.Tessellator;
-import net.minecraft.client.render.VertexFormats;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.LightType;
-import net.minecraft.world.SpawnHelper;
-import net.minecraft.world.World;
 
 public class LightLevelOverlay implements ClientModInitializer {
 
-    private static final String KEYBIND_CATEGORY = "key.lightleveloverlay.category";
+    private static final String KEYBIND_CATEGORY = "Light Level Overlay";
     private static final Identifier ENABLE_OVERLAY_KEYBIND = new Identifier("lightleveloverlay", "enable_overlay");
-    /*
-     * private static final BoundingBox TEST_BOX = new BoundingBox(0.6D / 2D, 0,
-     * 0.6D / 2D, 1D - 0.6D / 2D, 1D, 1D - 0.6D / 2D);
-     */
     private static FabricKeyBinding enableOverlay;
     private static boolean enabled = false;
-    // private static int reach = 12;
 
     public static final int POLLING_INTERVAL = 200;
     public static final int CHUNK_RADIUS = 3;
@@ -51,7 +34,7 @@ public class LightLevelOverlay implements ClientModInitializer {
         poller = new OverlayPoller();
         KeyBindingRegistryImpl.INSTANCE.addCategory(KEYBIND_CATEGORY);
         KeyBindingRegistryImpl.INSTANCE.register(enableOverlay = FabricKeyBinding.Builder
-                .create(ENABLE_OVERLAY_KEYBIND, InputUtil.Type.SCANCODE, 296, KEYBIND_CATEGORY).build());
+                .create(ENABLE_OVERLAY_KEYBIND, InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_G, KEYBIND_CATEGORY).build());
         ClothClientHooks.HANDLE_INPUT.register(minecraftClient -> {
             while (enableOverlay.wasPressed())
                 enabled = !enabled;
@@ -64,27 +47,10 @@ public class LightLevelOverlay implements ClientModInitializer {
                 if (player == null)
                     return;
 
-                //double d0 = camera.getPos().x;
-                //double d1 = camera.getPos().y - .005D;
-                //double d2 = camera.getPos().z;
-
-                double x = camera.getPos().x;//player.prevX + (player.x - player.prevX);// * event.getPartialTicks();
-                double y = camera.getPos().y - 0.005D;//player.prevY + (player.y - player.prevY);// * event.getPartialTicks();
-                double z = camera.getPos().z;//player.prevZ + (player.z - player.prevZ);// * event.getPartialTicks();
+                double x = camera.getPos().x;
+                double y = camera.getPos().y - 0.005D;
+                double z = camera.getPos().z;
                 renderer.render(x, y, z, poller.overlays);
-                /*
-                 * PlayerEntity playerEntity = client.player; World world = client.world;
-                 * GlStateManager.disableTexture(); GlStateManager.disableBlend(); BlockPos
-                 * playerPos = new BlockPos(playerEntity.x, playerEntity.y, playerEntity.z);
-                 * BlockPos.iterate(playerPos.add(-reach, -reach, -reach), playerPos.add(reach,
-                 * reach, reach)) .forEach(pos -> { if (world.getBiome(pos).getMaxSpawnLimit() >
-                 * 0) { CrossType type = getCrossType(pos, world, playerEntity); if (type !=
-                 * CrossType.NONE) { //VoxelShape shape =
-                 * world.getBlockState(pos).getCollisionShape(world, pos); Color color = type ==
-                 * CrossType.RED ? Color.RED : Color.YELLOW; renderCross(pos, color,
-                 * playerEntity); } } }); GlStateManager.enableBlend();
-                 * GlStateManager.enableTexture();
-                 */
             }
         });
     }
@@ -102,55 +68,8 @@ public class LightLevelOverlay implements ClientModInitializer {
         }
     }
 
-    public static CrossType getCrossType(BlockPos pos, World world, PlayerEntity playerEntity) {
-        BlockState blockBelowState = world.getBlockState(pos.down());
-        if (blockBelowState.getBlock() == Blocks.BEDROCK || blockBelowState.getBlock() == Blocks.BARRIER)
-            return CrossType.NONE;
-        if ((!blockBelowState.getMaterial().blocksLight() && blockBelowState.isTranslucent(world, pos.down()))
-                || !SpawnHelper.isClearForSpawn(world, pos, world.getBlockState(pos), world.getFluidState(pos)))
-            return CrossType.NONE;
-        if (blockBelowState.isAir() || !world.getBlockState(pos).isAir()
-                || !blockBelowState.hasSolidTopSurface(world, pos, playerEntity)
-                || !world.getFluidState(pos.down()).isEmpty())
-            return CrossType.NONE;
-        if (world.getLightLevel(LightType.BLOCK, pos) >= 8)
-            return CrossType.NONE;
-        if (world.getLightLevel(LightType.SKY, pos) >= 8)
-            return CrossType.YELLOW;
-        return CrossType.RED;
-    }
-
-    public static void renderCross(BlockPos pos, Color color, PlayerEntity entity) {
-        Camera camera = MinecraftClient.getInstance().gameRenderer.getCamera();
-        GlStateManager.lineWidth(1.0F);
-        GlStateManager.depthMask(false);
-        GlStateManager.disableTexture();
-        Tessellator tessellator = Tessellator.getInstance();
-        BufferBuilder buffer = tessellator.getBufferBuilder();
-        double d0 = camera.getPos().x;
-        double d1 = camera.getPos().y - .005D;
-        double d2 = camera.getPos().z;
-
-        buffer.begin(1, VertexFormats.POSITION_COLOR);
-        buffer.vertex(pos.getX() + .01 - d0, pos.getY() - d1, pos.getZ() + .01 - d2)
-                .color(color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha()).next();
-        buffer.vertex(pos.getX() - .01 + 1 - d0, pos.getY() - d1, pos.getZ() - .01 + 1 - d2)
-                .color(color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha()).next();
-        buffer.vertex(pos.getX() - .01 + 1 - d0, pos.getY() - d1, pos.getZ() + .01 - d2)
-                .color(color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha()).next();
-        buffer.vertex(pos.getX() + .01 - d0, pos.getY() - d1, pos.getZ() - .01 + 1 - d2)
-                .color(color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha()).next();
-        tessellator.draw();
-        GlStateManager.depthMask(true);
-        GlStateManager.enableTexture();
-    }
-
     public static boolean enabled() {
         return enabled;
-    }
-
-    private static enum CrossType {
-        YELLOW, RED, NONE
     }
 
 }
